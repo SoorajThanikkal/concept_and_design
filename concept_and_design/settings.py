f555a27967d06dbbf1f7ddda4cffd52409798ddd
sooraj <tpsooraj7@gmail.com>
2025-06-01 17:07:56 +0530

    changed debug false

--- concept_and_design/settings.py
+++ concept_and_design/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-457=zz2!^poy9i*!f#m10y2&1bsdyjw(hlo3e0h)gah)2pzx2)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
